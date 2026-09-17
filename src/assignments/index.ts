5b7cb198be1a2eb51e4cf774cc31cc6ef00a0aeb
Brian <bjin@wpi.edu>
2026-09-16 20:37:25 -0400

    Add week 4

--- src/assignments/index.ts
+++ src/assignments/index.ts
@@ -2,6 +2,7 @@ import type { ComponentType } from 'react';
 import { ResponsivePseudoScatterPlot } from './week-01/ResponsivePseudoScatterPlot';
 import { SummarizeDataset } from './week-02/SummarizeDataset';
 import { FirstVisual } from './week-03/FirstVisual';
+import { SecondPass } from './week-04/SecondPass';
 
 export interface Assignment {
   id: string;
@@ -24,6 +25,11 @@ export const assignments: Assignment[] = [
     id: '3',
     name: 'Week 3',
     component: FirstVisual,
+  },
+  {
+    id: '4',
+    name: 'Week 4',
+    component: SecondPass,
   }
 ];
 
